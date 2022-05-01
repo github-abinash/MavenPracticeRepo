c37d3da52a19eabced3282b9ddf2007082633607
abinash <mail.abi2022@gmail.com>
2022-05-02 00:26:44 +0530

    browser parameter added

--- src/test/java/JenkinsMaven/Project01/BrowserHandles.java
+++ src/test/java/JenkinsMaven/Project01/BrowserHandles.java
@@ -3,9 +3,11 @@ package JenkinsMaven.Project01;
 import org.junit.Assert;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.edge.EdgeDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
 import org.testng.annotations.AfterTest;
 import org.testng.annotations.BeforeTest;
+import org.testng.annotations.Parameters;
 import org.testng.annotations.Test;
 
 import com.fasterxml.jackson.databind.Module.SetupContext;
@@ -13,12 +15,23 @@ import com.fasterxml.jackson.databind.Module.SetupContext;
 import io.github.bonigarcia.wdm.WebDriverManager;
 
 public class BrowserHandles {
-   
+  @Parameters("BrowserInput")
   @Test
-  public void TestCase01()
+  public void TestCase01(String browserName)
   {
-	  WebDriverManager.chromedriver().driverVersion("101.0.4951.41").setup();
-	  WebDriver driver = new ChromeDriver();
+	  WebDriver driver=null;
+	  if(browserName.contains("Chrome"))
+	  {
+		  WebDriverManager.chromedriver().driverVersion("101.0.4951.41").setup();
+		  driver = new ChromeDriver();
+	  }
+	  else if(browserName.contains("Edge"))
+	  {
+		  WebDriverManager.edgedriver().driverVersion("101.0.1210.32").setup();
+		  driver = new EdgeDriver();
+	  }
+	  
+	  
 	  driver.get("https://www.google.co.in/");
 	  driver.manage().window().maximize();
 	  String title = driver.getTitle();
