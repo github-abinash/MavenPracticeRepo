f12cf8eb0c79ae8110706bca8200803cb1a3cc6c
abinash <mail.abi2022@gmail.com>
2022-05-02 15:55:20 +0530

    removed unnecessary import statements

--- src/test/java/JenkinsMaven/Project01/BrowserHandles.java
+++ src/test/java/JenkinsMaven/Project01/BrowserHandles.java
@@ -4,9 +4,6 @@ import org.junit.Assert;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.edge.EdgeDriver;
-import org.openqa.selenium.firefox.FirefoxDriver;
-import org.testng.annotations.AfterTest;
-import org.testng.annotations.BeforeTest;
 import org.testng.annotations.Parameters;
 import org.testng.annotations.Test;
 
