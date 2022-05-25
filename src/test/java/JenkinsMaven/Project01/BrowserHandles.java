8b7da4e29c44b96af1838a507b7cc60e699fc762
abinash <mail.abi2022@gmail.com>
2022-05-26 01:21:21 +0530

    from git bash

--- src/test/java/JenkinsMaven/Project01/BrowserHandles.java
+++ src/test/java/JenkinsMaven/Project01/BrowserHandles.java
@@ -4,28 +4,49 @@ import org.junit.Assert;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.edge.EdgeDriver;
+import org.testng.annotations.BeforeTest;
 import org.testng.annotations.Parameters;
 import org.testng.annotations.Test;
 
+import com.aventstack.extentreports.ExtentReports;
+import com.aventstack.extentreports.ExtentTest;
+import com.aventstack.extentreports.Status;
+import com.aventstack.extentreports.reporter.ExtentSparkReporter;
 import com.fasterxml.jackson.databind.Module.SetupContext;
 
 import io.github.bonigarcia.wdm.WebDriverManager;
 
-public class BrowserHandles {
+public class BrowserHandles 
+{
+	ExtentReports extent;
+	
+	@BeforeTest
+	public void Reports()
+	{
+		String reportLocation = System.getProperty("user.dir")+"\\reports";
+		ExtentSparkReporter reporter = new ExtentSparkReporter(reportLocation);
+		extent = new ExtentReports();
+		extent.attachReporter(reporter);
+	}
+	
+	
   @Parameters("BrowserInput")
   @Test
   public void TestCase01(String browserName)
-  {
+  { 
+	  ExtentTest logger = extent.createTest("TestCase01");
 	  WebDriver driver=null;
 	  if(browserName.contains("Chrome"))
 	  {
 		  WebDriverManager.chromedriver().driverVersion("101.0.4951.41").setup();
 		  driver = new ChromeDriver();
+		  logger.log(Status.INFO, "Chrome Browser Opened");
 	  }
 	  else if(browserName.contains("Edge"))
 	  {
 		  WebDriverManager.edgedriver().driverVersion("101.0.1210.32").setup();
 		  driver = new EdgeDriver();
+		  logger.log(Status.INFO, "Edge Browser Opened");
 	  }
 	  
 	  
